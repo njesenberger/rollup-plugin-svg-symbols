594be84547d604341db2d9820a37ba5533dd31fb
Nicolas Jesenberger <nicolas@MacBook-Air-de-Nicolas.local>
2023-12-15 13:42:43 +0100

    update: 0.0.21

--- rollup.config.js
+++ rollup.config.js
@@ -2,23 +2,26 @@ import typescript from '@rollup/plugin-typescript';
 import { defineConfig } from 'rollup';
 
 export default defineConfig({
-  input: ['./src/index.ts', './src/runtime.ts'],
-	output: [
-    {
-      entryFileNames: 'es/[name].mjs',
-      format: 'es',
-      dir: 'dist',
-    },
-    {
-      entryFileNames: 'cjs/[name].js',
-      format: 'cjs',
-      dir: 'dist',
-      exports: 'auto',
-    },
+	input: [
+		'./src/index.ts',
+		'./src/runtime.ts',
 	],
-  plugins: [
-    typescript(),
-  ],
+	output: [
+		{
+			entryFileNames: 'es/[name].mjs',
+			format: 'es',
+			dir: 'dist',
+		},
+		{
+			entryFileNames: 'cjs/[name].js',
+			format: 'cjs',
+			dir: 'dist',
+			exports: 'auto',
+		},
+	],
+	plugins: [
+		typescript(),
+	],
 	external: [
 		'@rollup/pluginutils',
 		'path',
